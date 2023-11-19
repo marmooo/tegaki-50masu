716fb216cd26b3f453754b75b5022148b7c75f20
marmooo <marmooo@users.noreply.github.com>
2023-11-19 10:34:44 +0900

    bump tfjs from 4.12.0 to 4.13.0

--- src/sw.js
+++ src/sw.js
@@ -10,7 +10,7 @@ const urlsToCache = [
   "/tegaki-50masu/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.12.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.13.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", (event) => {
